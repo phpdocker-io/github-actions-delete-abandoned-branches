38c1c070207a0520f4728c93411f3403c7223c0c
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-02-03 18:15:37 +0000

    Start specifying inputs and outputs

--- src/actions.py
+++ src/actions.py
@@ -1,5 +1,13 @@
 from src import github
 
 
-def run_action() -> None:
+def run_action() -> bool:
+    deleted_branches: list = ['foo']
     github.get_open_prs()
+
+    format_output(deleted_branches)
+    return True
+
+
+def format_output(deleted_branches: list) -> None:
+    print(f'::set-output deleted_branches={deleted_branches}')
