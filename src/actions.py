524fbd5754ca35d6e41c4d09c4e4a44a421cf33a
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-02-04 15:55:44 +0000

    Get github event from filesystem

--- src/actions.py
+++ src/actions.py
@@ -1,3 +1,5 @@
+import json
+
 from src.github import Github
 
 
@@ -18,14 +20,25 @@ def run_action(
 
     github = Github(github_repo=github_repo, github_token=github_token)
 
-    ref = github.get_branch_ref(github_event_path)
-    branches = github.get_deletable_branches(last_commit_age_days=last_commit_age_days, ignore_branches=ignore_branches)
+    github_event = get_github_event(github_event_path)
 
-    print(f"Branches queued for deletion: {branches}")
-    if dry_run is False:
-        print('This is NOT a dry run, deleting branches')
-        github.delete_branches(branches=branches)
-    else:
-        print('This is a dry run, skipping deletion of branches')
+    print(github_event)
+    # branches = github.get_deletable_branches(last_commit_age_days=last_commit_age_days, ignore_branches=ignore_branches)
+    #
+    # print(f"Branches queued for deletion: {branches}")
+    # if dry_run is False:
+    #     print('This is NOT a dry run, deleting branches')
+    #     github.delete_branches(branches=branches)
+    # else:
+    #     print('This is a dry run, skipping deletion of branches')
 
     return []
+
+
+def get_github_event(event_path: str) -> dict:
+    """
+    Github writes a file on the path given by event_path during a github action in our runtime. It has
+    certain info about what we're building.
+    """
+    with open(event_path) as json_file:
+        return json.load(json_file)
